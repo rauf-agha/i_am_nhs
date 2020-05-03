41298ca5546753a992ec72bd9944c3dbe3837b02
Ehtesham <etch20@gmail.com>
2020-05-03 01:03:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# i_am_nhs
+ NHS Heroes
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+              title: Center(child: Text('I am NHS')),
+              backgroundColor: Colors.blue),
+          body: Center(
+            child: Image(image: AssetImage('images/NHS.jpg')),
+          )),
+    ),
+  );
+}
